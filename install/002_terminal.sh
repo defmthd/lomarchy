52fd2734819288deb83428939afed3735113e311
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-07-24 20:57:37 +0300

    Enable Russian locale

--- install/002_terminal.sh
+++ install/002_terminal.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Enable Russian locale
+if grep '#ru_RU.UTF-8' /etc/locale.gen; then
+  sudo sed -i '/^#ru_RU.UTF-8 UTF-8/s/^#//' /etc/locale.gen
+  sudo locale-gen
+fi
+
 yay -S --noconfirm --needed \
   wget curl unzip inetutils \
   man tldr less \
   kitty \
   htop
+
+mkdir -p ~/.config
 
 cp -R ./config/kitty ~/.config/
