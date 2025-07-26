2e0352acd95dffba85dcee3f193404ae5f49d55d
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-07-26 20:18:18 +0300

    Add fontconfig

--- install/007_fonts.sh
+++ install/007_fonts.sh
@@ -4,3 +4,7 @@ yay -S --noconfirm --needed \
   ttf-ms-win11-auto ttf-jetbrains-mono-nerd \
   ttf-font-awesome noto-fonts noto-fonts-emoji \
   noto-fonts-cjk noto-fonts-extra
+
+if [ ! -d "$HOME/.config/fontconfig" ]; then
+  ln -s $(pwd)/config/fontconfig ~/.config/fontconfig
+fi
