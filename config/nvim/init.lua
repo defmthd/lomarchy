293e12ce8f8c17a008cd0529eef131e68367c774
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-01 21:02:00 +0300

    Change settings for translations in walker

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,14 +2,12 @@ vim.g.leader = ' '
 
 require("config.lazy")
 
-# Telescope shortcuts
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-# NvimTree shortcuts
 local nvim_tree_api = require "nvim-tree.api"
 vim.keymap.set('n', '<leader>e', function()
   nvim_tree_api.tree.toggle()
