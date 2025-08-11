44952630f0d266142f14af38c33e4ad684731704
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-11 22:51:20 +0300

    Add stylua

--- install/development/development.sh
+++ install/development/development.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 yay -S --noconfirm --needed visual-studio-code-bin github-desktop-bin \
-  github-cli lazygit
+  github-cli lazygit \
+  stylua
+
+if [ ! -d "$HOME/.config/stylua" ]; then
+  ln -s $(pwd)/config/stylua ~/.config/stylua
+fi
 
