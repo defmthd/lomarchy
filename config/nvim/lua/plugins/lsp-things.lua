cdc72a3e388d994b2aa1109f8eb3598100918376
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-11 23:54:03 +0300

    Add completions to nvim

--- config/nvim/lua/plugins/lsp-things.lua
+++ config/nvim/lua/plugins/lsp-things.lua
@@ -17,8 +17,12 @@ return {
   {
     'neovim/nvim-lspconfig',
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     end,
