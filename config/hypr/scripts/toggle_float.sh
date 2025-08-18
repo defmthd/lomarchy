e5275f662e6f78418120110dfc0b46f27be06a26
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-18 21:05:42 +0300

    Change commands order in toggle float

--- config/hypr/scripts/toggle_float.sh
+++ config/hypr/scripts/toggle_float.sh
@@ -4,8 +4,8 @@ IS_FLOATING=$(hyprctl activewindow -j | jq -r '.floating')
 
 if [[ "$IS_FLOATING" = "false" ]]; then
     hyprctl dispatch togglefloating
-    hyprctl dispatch centerwindow
     hyprctl dispatch resizeactive exact 80% 80%
+    hyprctl dispatch centerwindow
 else
     hyprctl dispatch togglefloating
 fi
