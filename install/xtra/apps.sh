1a7ec74669af6be94dce5f691167b5280bc78588
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-20 21:36:34 +0300

    Add chrome-flags.conf

--- install/xtra/apps.sh
+++ install/xtra/apps.sh
@@ -3,3 +3,7 @@
 yay -S --noconfirm --needed 1password-beta 1password-cli \
     google-chrome telegram-desktop obsidian \
     mpv imv evince
+
+if [ ! -f "$HOME/.config/chrome-flags.conf" ]; then
+  ln -s $(pwd)/config/chrome-flags.conf ~/.config/chrome-flags.conf
+fi
