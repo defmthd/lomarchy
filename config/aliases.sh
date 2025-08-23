ed766c07aae59fe4a350de790772836333543f82
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-23 16:24:13 +0300

    Add alias to download screensaver

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,3 +1,7 @@
 alias ul="yay -Suy --noconfirm --needed"
 alias ur="sudo reflector --verbose --country 'Russia' -l 20 --sort rate --save /etc/pacman.d/mirrorlist"
 
+# Download new screensaver
+# Example:
+#   dwn-ss https://www.youtube.com/watch?v=TwbW0iKofOw
+alias dwn-ss="yt-dlp --output='screensaver' --force-overwrites --format 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]' --cookies-from-browser chrome"
