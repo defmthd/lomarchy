4fc49b4513714a7e8dff78b87de08de3c09fb199
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-25 14:24:55 +0300

    Add aliases for yazi and nvim

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,3 +1,5 @@
+alias y="yazi"
+alias vim="nvim"
 alias ul="yay -Suy --noconfirm --needed"
 alias ur="sudo reflector --verbose --country 'Russia' -l 20 --sort rate --save /etc/pacman.d/mirrorlist"
 
@@ -5,3 +7,4 @@ alias ur="sudo reflector --verbose --country 'Russia' -l 20 --sort rate --save /
 # Example:
 #   dwn-ss https://www.youtube.com/watch?v=TwbW0iKofOw
 alias dwn-ss="yt-dlp --output='screensaver' --force-overwrites --format 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]' --cookies-from-browser chrome"
+
