22f5076f043ebcc71dd61a661b19e9211e8343e1
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-08-29 18:21:37 +0300

    Add syncthing

--- install/xtra/apps.sh
+++ install/xtra/apps.sh
@@ -2,8 +2,12 @@
 
 yay -S --noconfirm --needed 1password-beta 1password-cli \
     google-chrome telegram-desktop obsidian \
-    mpv imv evince
+    mpv imv evince \
+    syncthing
 
 if [ ! -f "$HOME/.config/chrome-flags.conf" ]; then
   ln -s $(pwd)/config/chrome-flags.conf ~/.config/chrome-flags.conf
 fi
+
+systemctl --user enable --now syncthing.service
+
