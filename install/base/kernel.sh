ad2c92d9b91d63b60e33d71062db3ec17f4f07b7
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-09-10 17:08:34 +0300

    Change scx to scx_loader

--- install/base/kernel.sh
+++ install/base/kernel.sh
@@ -3,5 +3,10 @@
 yay -S --noconfirm --needed \
   linux-cachyos linux-cachyos-headers scx-scheds
 
-sudo systemctl enable --now scx
+sudo systemctl enable --now scx_loader
+
+if [ ! -f /etc/scx_loader.toml ]; then
+  echo "default_sched = \"scx_lavd\"" | sudo tee -a /etc/scx_loader.toml > /dev/null
+  echo "default_mode = \"Gaming\"" | sudo tee -a /etc/scx_loader.toml > /dev/null
+fi
 
