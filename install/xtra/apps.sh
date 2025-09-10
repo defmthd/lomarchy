0e9fa3b0c7a2b377a8e1c9add9828a4aaa88cb56
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-09-10 18:06:12 +0300

    Install requirements for dictation

--- install/xtra/apps.sh
+++ install/xtra/apps.sh
@@ -9,5 +9,8 @@ if [ ! -f "$HOME/.config/chrome-flags.conf" ]; then
   ln -s $(pwd)/config/chrome-flags.conf ~/.config/chrome-flags.conf
 fi
 
+# Install requirement for dictation. See: hypr/scripts/record.py
+uv pip install --system faster-whisper sounddevice numpy
+
 systemctl --user enable --now syncthing.service
 
