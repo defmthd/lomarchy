185147e5055407c4d1d0b92453c9d31f9e80dee5
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-09-14 00:30:15 +0300

    Install language servers

--- install/development/vim.sh
+++ install/development/vim.sh
@@ -5,3 +5,7 @@ yay -S --noconfirm --needed neovim
 if [ ! -d "$HOME/.config/nvim" ]; then
   ln -s $(pwd)/config/nvim ~/.config/nvim
 fi
+
+yay -S --noconfirm lua-language-server bash-language-server
+
+gem install solargraph --no-document
