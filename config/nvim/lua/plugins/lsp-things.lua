8dd6ca8c1bfc97377859126337ed4720c6e0a201
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-09-14 00:29:27 +0300

    Simplify LSP config

--- config/nvim/lua/plugins/lsp-things.lua
+++ config/nvim/lua/plugins/lsp-things.lua
@@ -1,47 +1,10 @@
 return {
   {
-    'mason-org/mason.nvim',
-    opts = {},
+    "neovim/nvim-lspconfig",
     config = function()
-      require('mason').setup()
-    end,
-  },
-  {
-    'mason-org/mason-lspconfig.nvim',
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls', 'stylua', 'pyright', 'ruff' },
-      })
-    end,
-  },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-    end,
-  },
-  {
-    'nvimtools/none-ls.nvim',
-    config = function()
-      local null_ls = require('null-ls')
-
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.stylua,
-          null_ls.builtins.formatting.pyright,
-          null_ls.builtins.diagnostics.pyright,
-        },
-      })
-
-      vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-    end,
-  },
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('bashls')
+      vim.lsp.enable('solargraph')
+    end
+  }
 }
