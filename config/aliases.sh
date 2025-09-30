01f07e2279bf9b62089bfe44527dae761e2a29fd
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-09-30 15:04:58 +0300

    Add mount_nas function

--- config/aliases.sh
+++ config/aliases.sh
@@ -12,5 +12,15 @@ function ur () {
 # Download new screensaver
 # Example:
 #   dwn-ss https://www.youtube.com/watch?v=TwbW0iKofOw
-alias dwn-ss="yt-dlp --output='screensaver' --force-overwrites --format 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]' --cookies-from-browser chrome"
+# alias dwn-ss="yt-dlp --output='screensaver' --force-overwrites --format 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]' --cookies-from-browser chrome"
+
+function mount_nas () {
+  item=$(op item get "Colossus - DSM" --format json)
+  username=$(echo "$item" | jq -r '.fields[] | select(.id=="username") | .value')
+  password=$(echo "$item" | jq -r '.fields[] | select(.id=="password") | .value')
+
+  gio mount smb://$username:$password@192.168.1.69/Downloads
+  gio mount smb://$username:$password@192.168.1.69/Main
+  gio mount smb://$username:$password@192.168.1.69/Docker
+}
 
