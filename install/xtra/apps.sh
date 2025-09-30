d322bbc402dd9f4bb8c34f2b6f9395314fb6049d
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-09-30 14:05:00 +0300

    Add gnome calc

--- install/xtra/apps.sh
+++ install/xtra/apps.sh
@@ -3,7 +3,8 @@
 yay -S --noconfirm --needed 1password-beta 1password-cli \
     google-chrome telegram-desktop obsidian \
     mpv imv evince \
-    syncthing
+    syncthing \
+    gnome-calculator
 
 if [ ! -f "$HOME/.config/chrome-flags.conf" ]; then
   ln -s $(pwd)/config/chrome-flags.conf ~/.config/chrome-flags.conf
