dd8d5e36dc4aae082c80e92673f8ba68d6d0b14d
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-10-08 15:40:39 +0300

    Add toml plugin to vscode

--- install/development/vim.sh
+++ install/development/vim.sh
@@ -17,3 +17,4 @@ gem install solargraph solargraph-rails --no-document
 # code --install-extension anthropic.claude-code
 # code --install-extension mads-hartmann.bash-ide-vscode
 # code --install-extension castwide.solargraph
+# code --install-extension tamasfe.even-better-toml
