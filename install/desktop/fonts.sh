bfb5601ced65e914d0d63f18edf489c6b5b0c171
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-10-26 15:39:17 +0300

    Don't install Win fonts from aur

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yay -S --noconfirm --needed \
-  ttf-ms-win11-auto ttf-jetbrains-mono-nerd ttf-fantasque-nerd ttf-agave-nerd \
+  ttf-jetbrains-mono-nerd ttf-fantasque-nerd ttf-agave-nerd \
   ttf-font-awesome noto-fonts noto-fonts-emoji \
   noto-fonts-cjk noto-fonts-extra
 
