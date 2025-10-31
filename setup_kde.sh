4500e03d201a2d8311fed39bd57ceb9126167bca
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-10-31 19:35:13 +0300

    Use KDE apps for opening media files

--- setup_kde.sh
+++ setup_kde.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 yay -S --noconfirm --needed \
-    plasma-desktop konsole dolphin kscreen bluedevil plasma-nm qt5-tools qt6-tools
+    plasma-desktop konsole dolphin kscreen bluedevil plasma-nm qt5-tools qt6-tools \
+    gwenview okular
 
 source ./install/base/yay.sh
 source ./install/base/terminal.sh
@@ -18,3 +19,14 @@ source ./install/gaming/steam.sh
 source ./install/xtra/apps.sh
 source ./install/xtra/appsdef.sh
 source ./install/xtra/mimetypes.sh
+
+# Open all images with imv
+xdg-mime default org.kde.gwenview.desktop image/png
+xdg-mime default org.kde.gwenview.desktop image/jpeg
+xdg-mime default org.kde.gwenview.desktop image/gif
+xdg-mime default org.kde.gwenview.desktop image/webp
+xdg-mime default org.kde.gwenview.desktop image/bmp
+xdg-mime default org.kde.gwenview.desktop image/tiff
+
+# Open PDFs with the Document Viewer
+xdg-mime default org.kde.okular.desktop application/pdf
