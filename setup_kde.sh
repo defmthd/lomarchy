79ec3cff67a867f252f53cd3393e7b4f74b66e89
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-11-09 00:40:03 +0300

    Change install order

--- setup_kde.sh
+++ setup_kde.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed \
-    plasma-desktop konsole dolphin kscreen bluedevil plasma-nm qt5-tools qt6-tools \
-    gwenview okular
-
 source ./install/base/yay.sh
 source ./install/base/terminal.sh
 source ./install/base/kernel.sh
 source ./install/base/firmware.sh
 
+yay -S --noconfirm --needed \
+    plasma-desktop konsole dolphin kscreen bluedevil plasma-nm qt5-tools qt6-tools \
+    gwenview okular
+
+source ./install/desktop/audio.sh
 source ./install/desktop/fonts.sh
 source ./install/desktop/wine.sh
 
