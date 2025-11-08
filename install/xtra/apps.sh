42aaae692150d6320550432b1041a2ca6b3110a4
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-11-09 00:11:35 +0300

    Remove syncthing

--- install/xtra/apps.sh
+++ install/xtra/apps.sh
@@ -3,7 +3,6 @@
 yay -S --noconfirm --needed 1password 1password-cli \
     google-chrome telegram-desktop obsidian \
     mpv imv evince \
-    syncthing \
     gnome-calculator
 
 if [ ! -f "$HOME/.config/chrome-flags.conf" ]; then
@@ -14,4 +13,3 @@ fi
 # v pip install --system faster-whisper sounddevice numpy
 # uv pip install --system torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.4
 
-systemctl --user enable --now syncthing.service
