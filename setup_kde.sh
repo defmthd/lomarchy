1a6685f985f4c0307b0a793b1253624d0b4cab50
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-11-25 16:07:38 +0300

    Add more KDE stuff

--- setup_kde.sh
+++ setup_kde.sh
@@ -6,12 +6,18 @@ source ./install/base/kernel.sh
 source ./install/base/firmware.sh
 
 yay -S --noconfirm --needed \
-    plasma-desktop konsole dolphin ark kscreen bluedevil plasma-nm qt5-tools qt6-tools \
+    plasma-desktop konsole dolphin ark kscreen bluedevil plasma-pa plasma-nm qt5-tools qt6-tools \
     gwenview okular \
-    elementary-icon-theme papirus-icon-theme nwg-look
+    elementary-icon-theme papirus-icon-theme nwg-look \
+    sddm sddm-kcm \
+    xdg-desktop-portal xdg-desktop-portal-kde \
+    kde-gtk-config \
+    kwallet-pam
+
+sudo systemctl enable sddm.service
 
 source ./install/desktop/audio.sh
-source ./install/desktop/fonts.sh
+#source ./install/desktop/fonts.sh
 source ./install/desktop/wine.sh
 
 source ./install/development/development.sh
@@ -19,8 +25,8 @@ source ./install/development/development.sh
 source ./install/gaming/steam.sh
 
 source ./install/xtra/apps.sh
-source ./install/xtra/appsdef.sh
-source ./install/xtra/mimetypes.sh
+#source ./install/xtra/appsdef.sh
+#source ./install/xtra/mimetypes.sh
 
 # Open all images with imv
 xdg-mime default org.kde.gwenview.desktop image/png
