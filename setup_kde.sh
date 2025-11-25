706fd669333ed83f6be76d129f2e3c189f7207e0
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-11-25 22:41:54 +0300

    Refactor a bit

--- setup_kde.sh
+++ setup_kde.sh
@@ -26,9 +26,17 @@ source ./install/development/development.sh
 
 source ./install/gaming/steam.sh
 
-source ./install/xtra/apps.sh
-#source ./install/xtra/appsdef.sh
-#source ./install/xtra/mimetypes.sh
+yay -S --noconfirm --needed 1password 1password-cli \
+    google-chrome telegram-desktop obsidian
+
+if [ ! -f "$HOME/.config/chrome-flags.conf" ]; then
+  ln -s $(pwd)/config/chrome-flags.conf ~/.config/chrome-flags.conf
+fi
+
+# Use Chromium as the default browser
+xdg-settings set default-web-browser google-chrome.desktop
+xdg-mime default google-chrome.desktop x-scheme-handler/http
+xdg-mime default google-chrome.desktop x-scheme-handler/https
 
 # Open all images with imv
 xdg-mime default org.kde.gwenview.desktop image/png
