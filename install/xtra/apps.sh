ae47c5b0c93414cc121725ed5010695509d8aa64
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-11-26 14:29:29 +0300

    Remove comment

--- install/xtra/apps.sh
+++ install/xtra/apps.sh
@@ -8,8 +8,3 @@ yay -S --noconfirm --needed 1password 1password-cli \
 if [ ! -f "$HOME/.config/chrome-flags.conf" ]; then
   ln -s $(pwd)/config/chrome-flags.conf ~/.config/chrome-flags.conf
 fi
-
-# Install requirement for dictation. See: hypr/scripts/record.py
-# v pip install --system faster-whisper sounddevice numpy
-# uv pip install --system torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.4
-
