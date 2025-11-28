1fa68b36851fc394eac7dc4741fe61b577e51344
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-11-28 16:55:18 +0300

    Add kernel modules blacklist

--- install/base/kernel.sh
+++ install/base/kernel.sh
@@ -9,3 +9,14 @@ if [ ! -f /etc/scx_loader.toml ]; then
   echo "default_sched = \"scx_lavd\"" | sudo tee -a /etc/scx_loader.toml > /dev/null
   echo "default_mode = \"Gaming\"" | sudo tee -a /etc/scx_loader.toml > /dev/null
 fi
+
+# Remove from kernel some legacy
+sudo sh -c 'cat > /etc/modprobe.d/local-blacklist.conf << EOF
+# printer legacy
+blacklist usblp
+blacklist lpc_ich
+
+# input legacy
+blacklist mousedev
+blacklist joydev
+EOF'
