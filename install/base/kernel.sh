8b33724c61f93788a7e7c59e5ba6004a8dd8096d
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-11-28 17:05:24 +0300

    Add check for already existing local blacklist

--- install/base/kernel.sh
+++ install/base/kernel.sh
@@ -10,8 +10,9 @@ if [ ! -f /etc/scx_loader.toml ]; then
   echo "default_mode = \"Gaming\"" | sudo tee -a /etc/scx_loader.toml > /dev/null
 fi
 
-# Remove from kernel some legacy
-sudo sh -c 'cat > /etc/modprobe.d/local-blacklist.conf << EOF
+# Remove from kernel some legacy modules
+if [ ! -f /etc/modprobe.d/local-blacklist.conf ]; then
+  sudo sh -c 'cat > /etc/modprobe.d/local-blacklist.conf << EOF
 # printer legacy
 blacklist usblp
 blacklist lpc_ich
@@ -20,3 +21,4 @@ blacklist lpc_ich
 blacklist mousedev
 blacklist joydev
 EOF'
+fi
