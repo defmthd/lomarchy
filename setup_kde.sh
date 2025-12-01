0a3d86c3bdff5828d006c71d3b7ab70b98a505c3
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-12-01 14:00:58 +0300

    Add spectacle

--- setup_kde.sh
+++ setup_kde.sh
@@ -8,7 +8,7 @@ source ./install/base/firmware.sh
 
 yay -S --noconfirm --needed \
     plasma-desktop konsole dolphin ark kscreen bluedevil plasma-pa plasma-nm qt5-tools qt6-tools \
-    gwenview okular \
+    gwenview okular spectacle \
     elementary-icon-theme papirus-icon-theme nwg-look \
     sddm sddm-kcm \
     xdg-desktop-portal xdg-desktop-portal-kde \
