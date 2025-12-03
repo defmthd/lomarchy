7e9ed472562055e26f683ef363bfb076e003f7bc
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-12-03 21:52:08 +0300

    Add Segoe UI font

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -2,6 +2,7 @@
 
 yay -S --noconfirm --needed \
   ttf-jetbrains-mono-nerd \
+  ttf-segoe-ui-variable \
   ttf-font-awesome noto-fonts noto-fonts-emoji
 
 if [ ! -d "$HOME/.config/fontconfig" ]; then
