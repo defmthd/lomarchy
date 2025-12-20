a7c0671504d5bc307a6e4fa3662e36592e59f002
Alexander Lomakin <alexander.lomakin@gmail.com>
2025-12-20 21:52:03 +0300

    Mkinitcpio move inside if

--- install/base/modprobe.sh
+++ install/base/modprobe.sh
@@ -4,6 +4,5 @@ echo '---> modprobe'
 
 if [ ! -f "/etc/modprobe.d/legacy.conf" ]; then
   sudo cp $(pwd)/config/modprobe/legacy.conf /etc/modprobe.d/legacy.conf
+  sudo mkinitcpio -P
 fi
-
-sudo mkinitcpio -P
