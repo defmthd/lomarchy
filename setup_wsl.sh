4870ee22f783357ae97d8325337e0e3c2163307a
Alexander Lomakin <alexander.lomakin@gmail.com>
2026-01-19 20:29:07 +0300

    Add setup script for WSL

---
+++ setup_wsl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ~/.config
+
+source ./install/base/locale.sh
+source ./install/base/sudoers.sh
+source ./install/base/yay.sh
+
+source ./install/base/zsh.sh
+source ./install/base/commandline.sh
+source ./install/base/alia.sh
