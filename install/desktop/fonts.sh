07b266a766764544768e9798a73306cbada2da06
Alexander Lomakin <alexander.lomakin@gmail.com>
2026-01-27 18:18:33 +0300

    New font for ghostty

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -3,7 +3,8 @@
 yay -S --noconfirm --needed \
   ttf-segoe-ui-variable \
   ttf-jetbrains-mono-nerd \
-  ttf-font-awesome noto-fonts noto-fonts-emoji
+  ttf-font-awesome noto-fonts noto-fonts-emoji \
+  ttf-fixedsys-excelsior-linux
 
 if [ ! -d "$HOME/.config/fontconfig" ]; then
   ln -s $(pwd)/config/fontconfig ~/.config/fontconfig
